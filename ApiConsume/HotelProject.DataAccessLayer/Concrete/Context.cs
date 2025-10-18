0f4d43945d7bb272f7c16cc89bbf9cb0578e3690
Umutmert1903 <umutmertof4@gmail.com>
2025-10-18 19:59:39 +0300

    Bölüm13:

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace HotelProject.DataAccessLayer.Concrete
 {
-    public class Context: IdentityDbContext<AppUser, AppRole, int>
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -18,10 +18,10 @@ namespace HotelProject.DataAccessLayer.Concrete
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Service> Services { get; set; }
         public DbSet<Staff> Staffs { get; set; }
-        public DbSet<Subscribe>Subscribes { get; set; }
-        public DbSet<Testimonial>Testimonials { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<About> Abouts { get; set; }
-
         public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Guest> Guests {get; set;}
     }
 }
