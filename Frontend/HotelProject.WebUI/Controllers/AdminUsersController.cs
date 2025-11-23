eab57b822f3db50feb9676b2872abf6e0ed5b0e3
yasindenizcure <yasindenizcure@gmail.com>
2025-11-23 08:46:44 +0300

    Bölüm 27: Rol Ekleme, Silme, Güncelleme İşlemleri

--- Frontend/HotelProject.WebUI/Controllers/AdminUsersController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminUsersController.cs
@@ -17,19 +17,6 @@ namespace HotelProject.WebUI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-
-        //public async Task<IActionResult> Index()
-        //{
-        //    var client = _httpClientFactory.CreateClient();
-        //    var responseMessage = await client.GetAsync("http://localhost:5088/api/AppUser");
-        //    if (responseMessage.IsSuccessStatusCode)
-        //    {
-        //        var jsonData = await responseMessage.Content.ReadAsStringAsync();
-        //        var values = JsonConvert.DeserializeObject<List<ResultAppUserDto>>(jsonData);
-        //        return View(values);
-        //    }
-        //    return View();
-        //}
         public async Task<IActionResult> UserList() 
         {
             var client = _httpClientFactory.CreateClient();
